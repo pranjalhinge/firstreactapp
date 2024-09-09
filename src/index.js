8dfd65364afd1002bbbb0e3e6d5eb85dbc21a6d1
pranjalhinge <pranjalhinge4@gmail.com>
2024-09-09 08:21:04 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './view/app/App';
 import Navs from './component/navbar/nav';
-import Imgss from './wallpaperflare.com_wallpaper (3).jpg'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const Hey = window.location.pathname ;
 if (Hey === '/'){
   root.render(<>
   
-  <div className='center'>
- <h1>home</h1>
-    <img src={Imgss} alt="hii" className='helloimg' />
+  <div>
+ <h1>Hello World</h1>
     </div>
   </>
 );
